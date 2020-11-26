e9418cc86f2d2a13e3d491e8deecc9ad95dea997
Rodolfo Sarmiento <RXS0PRA@lt352476.lan>
2020-11-26 16:44:23 -0500

    from rest to graphql

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import {schema} from './src/data/schema'
+
+const app = express();
+const PORT = 8080;
+
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`GraphQL is running`)
+);
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}))
+app.listen(PORT, () =>
+    console.log(`your server is running on port http://localhost:${PORT}/graphql`)
+);
---
+++ src/data/dbConnectors.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+//Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost', {
+    useNewUrlParser: true
+});
+
+
+
+ const contactSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    }
+});
+
+const Contacts = mongoose.model('contacts', contactSchema);
+
+export { Contacts }
